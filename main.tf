1f36eecee743e15a24f2c8152a4aba2f3eadcf1d
Vijayabalan Balakrishnan <bvijaycom@users.noreply.github.com>
2023-02-17 13:12:47 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,30 @@
-resource "aws_instance" "my_vm" {
-  ami           = "ami-00874d747dde814fa" //Ubuntu AMI
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_key_pair" "example" {
+  key_name   = "examplekey"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "example" {
+  key_name      = aws_key_pair.example.key_name
+  ami           = "ami-0b5eea76982371e91"
   instance_type = "t2.micro"
 
-  tags = {
-    Name = "My EC2 instance",
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo amazon-linux-extras enable nginx1.12",
+      "sudo yum -y install nginx",
+      "sudo systemctl start nginx"
+    ]
   }
 }
